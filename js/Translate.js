fb8213c0c62849073cf44b22b6558fc454b79ebc
olesjothun <ole@sjothun.net>
2024-08-03 18:15:02 +0200

    Fixed async Axios

--- js/Translate.js
+++ js/Translate.js
@@ -2,17 +2,21 @@
 let translations;
 
 async function getTranslation(path) {
-    try {
+    try {/* Fetch working OK
         const response = await fetch(path);
         if (!response.ok) {
             throw new Error(`Response status: ${response.status}`);
         }
+        translations = await response.json();*/
 
-        translations = await response.json();
+        let result = await axios.get(path);
+        if (result.status == 200) translations = result.data;
+        else alert("Error in Axios get: " + result.status);
 
+        //if (res.status == 200) getResult(res);
 
-
-            /*
+        //GetAxios(path).then(result => translations = result.data);
+            /* Async not working...
         axios.get(path).then(
             (response) => {
                 translations = response.data;
@@ -28,3 +32,17 @@ async function getTranslation(path) {
         alert(error);
     }
 }
+
+/*
+async function GetAxios(path) {
+    let res = await axios.get(path);
+    return res;
+
+}
+*/
+
+/*
+function getResult(res) {
+    translations = res.data;
+}
+*/
